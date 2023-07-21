3f3b496c794885ab86bc177f430d51f9bf38364a
quynhnt <trangquynh17@gmail.com>
2023-07-21 16:40:39 +0700

    restructure

--- server/dist/user/user.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './schemas/user.schema';
-import { Model } from 'mongoose';
-import { JwtService } from '@nestjs/jwt';
-export declare class UserService {
-    private userModel;
-    private jwtService;
-    constructor(userModel: Model<User>, jwtService: JwtService);
-    getUser(request: any): any;
-}
