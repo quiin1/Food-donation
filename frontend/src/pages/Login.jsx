889ea39b8b5fec5e2c5c8b6eba276ad31b874fb9
quynhnt <trangquynh17@gmail.com>
2023-07-21 17:17:21 +0700

    restructure

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,57 @@
+import { React } from 'react'
+import { Link, Typography, Box, CardMedia, CardContent, Grid } from '@mui/material';
+import { Navigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { defaultStyle, backgroundStyle, flexCenter, textStyle0, textStyle3, palette } from '../theme';
+import { LoginForm } from '../components/LoginForm';
+import { SignupForm } from '../components/SignupForm';
+import loginSlice from '../redux/loginSlice';
+import { LOGIN, SIGNUP } from '../until/constants'
+import { statusLoginSelector } from '../redux/selectors';
+import { authenticatedSelector } from '../redux/selectors';
+
+function Login() {
+  const dispatch = useDispatch()
+  let authenticated = useSelector(authenticatedSelector)
+  const status = useSelector(statusLoginSelector)
+
+  if (authenticated) {
+    return <Navigate to={"/dashboard"}/>
+  }
+
+  function handleStatusChange(e) { // switch LOGIN SIGNUP
+    e.preventDefault()
+    const nextStatus = status === LOGIN ? SIGNUP : LOGIN
+    dispatch(loginSlice.actions.changeStatus(nextStatus))
+  }
+  
+  return (
+    <Grid container sx={defaultStyle}>
+      <Grid item xs={5} md={8} sx={backgroundStyle}/> 
+      <Grid item xs={7} md={4} sx={flexCenter}>
+        <Box maxWidth={'70%'}>
+          <Box>
+            <Typography align="center" sx={textStyle0} marginBottom={'.5em'}>
+                Welcome to StartNow
+            </Typography>
+            <CardMedia component="img" image='/login/humanitarian-day1.png' />
+          </Box>
+
+          <CardContent>
+            {status === LOGIN? 
+              <LoginForm/> : 
+              <SignupForm/>}
+            <Box>
+                <Link href="/" onClick={e => handleStatusChange(e)} underline="none" sx={textStyle3} color="#2BA84A">Click here</Link>
+                <Typography display="inline" ml={.5} sx={textStyle3} color={palette.black}>
+                  to {status === LOGIN ? 'Sign up' : 'Login'} if you've already have an account
+                </Typography>
+            </Box>
+          </CardContent>
+        </Box>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Login
---
+++ frontend/src/until/constants.js
@@ -0,0 +1,35 @@
+export const LOGIN = 'LOGIN'
+export const SIGNUP = 'SIGNUP'
+export const LOGIN_SUCCESSFULLY = true
+export const LOGOUT = 'LOGOUT';
+
+// dashboard nav left bar
+export const listNavItems = [
+    {
+      title: "Overview",
+      img: "/dashboard.svg",
+    },
+    {
+      title: "Post manager",
+      img: "/fi-sr-document-signed.svg",
+    },
+    {
+      title: "Location",
+      img: "/fi-sr-location-alt.svg",
+    },
+    {
+      title: "Reward",
+      img: "/fi-sr-hand-holding-heart.svg",
+    },
+    {
+      title: "Payment record",
+      img: "/fi-sr-money.svg",
+    }
+  ]
+
+const baseUrl = 'http://localhost:5000'
+export const api = {
+  LOGIN: baseUrl + '/auth/login',
+  SIGNUP: baseUrl + '/auth/register',
+  GET_ACCOUNTS: baseUrl + '/api/v1/accounts',
+}
