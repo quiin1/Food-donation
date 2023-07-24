3c3b1a75fa58e1c24b1bc93e05150327882e5c0e
quynhnt <trangquynh17@gmail.com>
2023-07-24 17:20:57 +0700

    feat: handleOpen ActionForm from Overview

--- frontend/src/subpages/Dashboard/Overview.tsx
+++ frontend/src/subpages/Dashboard/Overview.tsx
@@ -1,17 +1,39 @@
-import React from 'react'
-import { useDispatch } from 'react-redux';
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { Box, Typography, Divider, Grid } from '@mui/material'
 import PlaceIcon from '@mui/icons-material/Place';
 
 import MyCard from '../../components/Dashboard/Overview/MyCard'
+import ActionForm from '../../components/Dashboard/AddPost/ActionForm';
 
 import dashboardSlice from '../../redux/dashboardSlice';
 import * as dashboard from '../../until/constants';
+import { dataSelector } from '../../redux/selectors';
+import { subpageIndexSelector } from '../../redux/selectors';
 
 const Overview: React.FC = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
+
+    let data = useSelector(dataSelector)
+    const [formIndex, setFormIndex] = useState(1)
+
+    const [open, setOpen] = useState(false)
+    const handleOpen = (id: number) => { 
+        // switch(id) {
+        //     case 1: 
+        //     case 2: 
+        //     case 3: 
+        //     case 4: 
+        //         dispatch(dashboardSlice.actions.changeSubpage(dashboard.listNavItems[id].title))
+        //         break
+        //     default:
+        //         dispatch(dashboardSlice.actions.changeSubpage("Overview"))
+        // }
+        if (id > 0 && id < 4) setFormIndex(id)
+        setOpen(true)
+    }
 
     const handleNavigate = (id: number) => { 
         switch(id) {
@@ -26,7 +48,7 @@ const Overview: React.FC = () => {
                 dispatch(dashboardSlice.actions.changeSubpage("Overview"))
         }
     }
-    const data = [
+    const cardData = [
         {
             id: 1,
             title: "Add a new post",
@@ -56,38 +78,42 @@ const Overview: React.FC = () => {
             img: dashboard.listNavItems[4].img
         },
     ]
-  return (
-    <Box>
-        <Typography sx={{
-            color: "#141416",
-            fontWeight: "600",
-            fontSize: "1.5em"  
-        }}>
-            Welcome back, {localStorage.getItem("username")}
-        </Typography>
-        <Box className="description" display="flex" sx={{margin: ".8em 0 1em 0"}}>
-            <PlaceIcon sx={{
-                color: "#2BA84A",
-                mr: ".2em"
-            }}/>
-            <Typography>South Austraa(SA), 5583</Typography>
+
+    return (
+        <Box>
+            <ActionForm open={open} setOpen={setOpen} data={data[formIndex].actionForm} i={formIndex}/>
+            
+            <Typography sx={{
+                color: "#141416",
+                fontWeight: "600",
+                fontSize: "1.5em"  
+            }}>
+                Welcome back, {localStorage.getItem("username")}
+            </Typography>
+            <Box className="description" display="flex" sx={{margin: ".8em 0 1em 0"}}>
+                <PlaceIcon sx={{
+                    color: "#2BA84A",
+                    mr: ".2em"
+                }}/>
+                <Typography>South Austraa(SA), 5583</Typography>
+            </Box>
+            <Divider/>
+            <Grid container spacing={4} mt=".3em">
+                {cardData.map(item => (
+                    <MyCard 
+                        key={item.title}
+                        title={item.title}
+                        content={item.content}
+                        btnName={item.btnName}
+                        // handleOpen={() => handleNavigate(item.id)}
+                        handleOpen={() => handleOpen(item.id)}
+                    >
+                        <img style={{width:"2em", filter: "invert(58%) sepia(22%) saturate(1478%) hue-rotate(82deg) brightness(87%) contrast(85%)"}} src={item.img}/>
+                    </MyCard>
+                ))}
+            </Grid>
         </Box>
-        <Divider/>
-        <Grid container spacing={4} mt=".3em">
-            {data.map(item => (
-                <MyCard 
-                    key={item.title}
-                    title={item.title}
-                    content={item.content}
-                    btnName={item.btnName}
-                    handleOpen={() => handleNavigate(item.id)}
-                >
-                    <img style={{width:"2em", filter: "invert(58%) sepia(22%) saturate(1478%) hue-rotate(82deg) brightness(87%) contrast(85%)"}} src={item.img}/>
-                </MyCard>
-            ))}
-        </Grid>
-    </Box>
-  )
+    )
 }
 
 export default Overview
