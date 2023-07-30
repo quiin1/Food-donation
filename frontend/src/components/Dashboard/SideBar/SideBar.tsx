e7ccf53181ec31aebc1d786b90f22ab2586f102a
quynhnt <trangquynh17@gmail.com>
2023-07-31 06:22:59 +0700

    fix: change subpage base on url for go back and forward

--- frontend/src/components/Dashboard/SideBar/SideBar.tsx
+++ frontend/src/components/Dashboard/SideBar/SideBar.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Box, CardMedia, Typography, List } from "@mui/material"
 import { palette } from '../../../theme'
 import logo from '../../../assets/logo/logo.png'
 import { listNavItems } from '../../../until/constants';
 import NavItem from './NavItem'
-import { subpageSelector } from '../../../redux/selectors';
 import dashboardSlice from '../../../redux/dashboardSlice';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const SideBar: React.FC = () => {
-    const navigate = useNavigate();
+    const navigate = useNavigate()
     const dispatch = useDispatch()
-    const subpage = useSelector(subpageSelector)
+    const location = useLocation()
+    const subpage = location.pathname
 
     function handleChangeSubpage(title: string, index: number) {
         switch (index) {
@@ -45,7 +45,7 @@ const SideBar: React.FC = () => {
                 {listNavItems.map((item, index) => (
                     <NavItem id={index} key={index} title={item.title} 
                         onClick={() => handleChangeSubpage(item.title, index)}
-                        className={subpage === item.title ? "chosen" : ""}
+                        className={subpage === item.path ? "chosen" : ""}
                     >
                     <CardMedia sx={{width: "22px"}} component="img" src={item.img}/>
                     </NavItem>
