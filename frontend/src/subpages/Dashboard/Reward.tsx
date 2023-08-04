20c6488f90e1c2570adc4f14b95ad751a3180d50
quynhnt <trangquynh17@gmail.com>
2023-08-04 14:59:50 +0700

    feat: display crud post for other subpages

--- frontend/src/subpages/Dashboard/Reward.tsx
+++ frontend/src/subpages/Dashboard/Reward.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Grid } from '@mui/material'
-import { GridActionsCellItem, GridPaginationModel } from '@mui/x-data-grid'
+import { Box, Button, Dialog, DialogActions, DialogTitle, Grid } from '@mui/material'
+import { GridActionsCellItem } from '@mui/x-data-grid'
+import { useSnackbar } from 'notistack';
 import DeleteIcon from '@mui/icons-material/Delete';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import titleImage from "../../assets/dashboard/title/3.svg"
+import EditIcon from '@mui/icons-material/Edit';
+import IconEye from '../../assets/dashboard/table/fi-sr-eye.svg';
+import titleImage from "../../assets/dashboard/title/1.png"
 
 import Table from '../../components/Dashboard/Table/Table'
 import Dashboard from '../../pages/Dashboard';
@@ -12,8 +14,11 @@ import ActionForm from '../../components/Dashboard/CRUDPost/ActionForm';
 import { useSelector } from 'react-redux';
 import { dataSelector } from '../../redux/selectors';
 import ActionInfoInputs from '../../components/Dashboard/CRUDPost/ActionInfoInputs';
-import { useSnackbar } from 'notistack';
-import { deletePost, getAllPosts, postCreatePost, updatePost } from '../../api';
+import { postCreatePost, deletePost, updatePost, getPosts, getAllPosts } from '../../api';
+import 'firebase/compat/firestore';
+import { storage } from '../../App';
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { useLocation } from 'react-router-dom';
 
 const Reward: React.FC = () => {
     const columns = [
@@ -38,7 +43,7 @@ const Reward: React.FC = () => {
                         alignItems: 'center',
                         gap: '1em'
                     }}>
-                        <img src={params.row.img || ""} alt={""}/>
+                        <img style={{ maxWidth: "32px" }} src={params.row.img} alt={"title-image"} />
                         {params.row.voucherInfo || ""}
                     </Box>
                 )
@@ -83,56 +88,89 @@ const Reward: React.FC = () => {
                     onClick={() => handleDelete(params.id)}
                 />,
                 <GridActionsCellItem
-                icon={<MoreHorizIcon sx={{color: "#84818A", width: ".8em" }}/>}
+                icon={<EditIcon sx={{color: "#84818A", width: ".8em" }}/>}
                 label="Setting"
                 />,
             ],
         },
     ]
-    const [rows, setRows] = useState([
-        { id: 9256821912, img: titleImage, voucherInfo: 'VOUCHER 50%', expiredDate: '15:46.673 02/08/2022', activedDate: '15:46.673 02/08/2022', status: 'Active' },
-    ])
+    // const [rows, setRows] = useState([
+    //     { id: 9256821912, img: titleImage, voucherInfo: 'VOUCHER 50%', expiredDate: '15:46.673 02/08/2022', activedDate: '15:46.673 02/08/2022', status: 'Active' },
+    // ])
+
+    const [rows, setRows] = useState<any>([])
 
     let data = useSelector(dataSelector)
-    const [open, setOpen] = useState(false)
-    const handleOpen = () => { 
-        setOpen(true)
-    }
-    function handleClose() {
-        setOpen(false)
-    }
+    const location = useLocation();
+    const [openActionForm, setOpenActionForm] = useState(location?.state?.openActionForm || false)
+    const [openConfirmDelete, setOpenConfirmDelete] = useState(false)
+
     const [title, setTitle] = useState('')
-    
-    const { enqueueSnackbar } = useSnackbar() 
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [selectedFileURL, setSelectedFileURL] = useState('');
+
+    const { enqueueSnackbar } = useSnackbar()
     const [openSuccess, setOpenSuccess] = useState(false)
-    const [loading, setLoading] = useState(true)
     const [isUpdateData, setIsUpdateData] = useState(false)
     const [idToUpdate, setIdToUpdate] = useState(-1)
+    const [idToDelete, setIdToDelete] = useState(-1)
     const [_idToUpdate, set_IdToUpdate] = useState(-1)
 
-    // useEffect(() => {
-    //     getAllPosts(setRows, setLoading)
-    // }, [])
+    const [paginationModel, setPaginationModel] = useState({
+        page: 1,
+        pageSize: 8
+    })
+    const [loading, setLoading] = useState(true)
+    const [totalRows, setTotalRows] = useState(0)
+
+    useEffect(() => {
+        // getAllPosts(setRows, setLoading)
+        getPosts(paginationModel, setRows, setTotalRows, setLoading)
+    }, [paginationModel])
+
+    useEffect(() => {
+        updateSelectedFileURL()
+    }, [selectedFile, setSelectedFile])
+
+    async function updateSelectedFileURL() {
+        if (selectedFile) {
+            const imageRef = ref(storage, `/${selectedFile.name}`);
+            const uploadedImage = await uploadBytes(imageRef, selectedFile).then((snapshot: { ref: any; }) => {
+                return getDownloadURL(snapshot.ref)
+            })
+            // console.log(uploadedImage)
+            setSelectedFileURL(uploadedImage)
+        }
+        else {
+            setSelectedFileURL('')
+        }
+    }
 
     function refreshValues() {
         setTitle('')
+        setSelectedFile(null)
+        setIdToDelete(-1)
     }
 
     function handleSubmit() {
-        console.log("isUpdateData", isUpdateData)
+        // console.log("isUpdateData", isUpdateData)
         // check Empty 
         if (title === '') {
-            enqueueSnackbar('Please fill the Title field', {variant: "error"});
+            enqueueSnackbar('Please fill the Title field', { variant: "error" });
             return
         }
-        handleClose()
+        if (!selectedFileURL) {
+            enqueueSnackbar('Please upload image!', { variant: "error" });
+            return
+        }
+        setOpenActionForm(false)
 
         /** 
          * Add new Data
          * func: handleAdd      call API 
         */
         if (!isUpdateData) handleAdd()
-        
+
         /**
          * Update data
          * func: updatePost     call API put
@@ -140,18 +178,19 @@ const Reward: React.FC = () => {
         else {
             if (_idToUpdate !== -1) {
                 updatePost(_idToUpdate, {
-                    title
+                    title,
+                    img: selectedFileURL
                 })
                 setIsUpdateData(false)
                 setIdToUpdate(-1)
                 set_IdToUpdate(-1)
-                
+
                 const updatedRow = {
                     id: rows[idToUpdate].id,
-                    img: rows[idToUpdate].img || titleImage,
+                    img: selectedFileURL || titleImage,
                     title,
-                    // releaseDate: rows[idToUpdate].releaseDate, 
-                    view: 200, 
+                    releaseDate: rows[idToUpdate].releaseDate,
+                    view: 200,
                     status: 'Online'
                 }
                 const newRows = rows.map((item: any, index: number) =>
@@ -163,68 +202,75 @@ const Reward: React.FC = () => {
 
         /**
          * *** SUCCESSFULLY ***
-         *  */ 
+         *  */
         setOpenSuccess(true)
-        enqueueSnackbar('Create successfully!', {variant: "success"});
-        
+        enqueueSnackbar('Create successfully!', { variant: "success" });
+
         // refresh values 
         refreshValues()
     }
 
-    const handleAdd = () => {
+    const handleAdd = async () => {
+        // console.log("selectedFile", selectedFile)
+        // console.log("selectedFileURL", selectedFile)
+
         const currentDate = new Date();
         const dateString = currentDate.toISOString();
         // FE
         const newRow = {
             id: Math.round(Math.random() * 9000000000),
-            img: titleImage,
+            img: selectedFileURL,
             title,
-            releaseDate: dateString, 
-            view: 200, 
+            releaseDate: dateString,
+            view: 200,
             status: 'Online'
         }
-        // setRows([...rows, newRow])
+        setRows([newRow, ...rows])
         // BE
         const storeNewRow = {
             id: newRow.id,
+            img: selectedFileURL,
             title,
             releaseDate: dateString,
             view: 200,
             status: 'Online'
         }
         postCreatePost(storeNewRow)
+        getPosts(paginationModel, setRows, setTotalRows, setLoading)
     }
 
     const handleDelete = (id: number) => {
         const indexToRemove = rows.findIndex((item: any) => item.id === id)
-        console.log(indexToRemove)
-        if (indexToRemove >= 0 && indexToRemove < rows.length) {
+        if (indexToRemove >= 0 && indexToRemove < rows.length && idToDelete !== -1) {
+            /** Delete */
             const newRows = rows.filter((_: any, index: any) => index !== indexToRemove)
             setRows(newRows)
             /** 
              * use API
              */
-            // deletePost(rows[indexToRemove]._id)
+            deletePost(rows[indexToRemove]._id)
         } else {
             console.log('Invalid index. No object was removed.');
         }
+        refreshValues()
+        getPosts(paginationModel, setRows, setTotalRows, setLoading)
     }
-    
+
     const handleEdit = (id: number) => {
         const indexToEdit = rows.findIndex((item: any) => item.id === id)
         setIsUpdateData(true)
         setIdToUpdate(indexToEdit)
-        // set_IdToUpdate(rows[indexToEdit]._id)
+        set_IdToUpdate(rows[indexToEdit]._id)
 
-        
         setIdToUpdate(indexToEdit)
         if (indexToEdit >= 0 && indexToEdit < rows.length) {
             /**
              * front-end
              * >> get current data
              */
-            // setTitle(rows[indexToEdit].title)
-            handleOpen()            
+            setTitle(rows[indexToEdit].title)
+            setSelectedFileURL(rows[indexToEdit].img || titleImage)
+            setOpenActionForm(true)
         } else {
             console.log('Invalid index');
         }
@@ -232,33 +278,50 @@ const Reward: React.FC = () => {
 
     return (
         <Dashboard>
-            <ActionForm 
-                open={open}
-                setOpen={setOpen}
+            <Dialog
+                open={openConfirmDelete}
+                onClose={() => setOpenConfirmDelete(false)}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Are you sure to delete this post?"}
+                </DialogTitle>
+                <DialogActions>
+                    <Button onClick={() => {handleDelete(idToDelete); setOpenConfirmDelete(false);}} autoFocus>
+                        Agree
+                    </Button>
+                    <Button onClick={() => setOpenConfirmDelete(false)}>Cancel</Button>
+                </DialogActions>
+            </Dialog>
+            <ActionForm
+                open={openActionForm}
+                setOpen={setOpenActionForm}
                 data={data[3].actionForm}
                 i={3}
                 openSuccess={openSuccess}
                 setOpenSuccess={setOpenSuccess}
-                handleCloseActionForm={handleClose}
+                handleCloseActionForm={() => setOpenActionForm(false)}
                 handleSubmit={handleSubmit}
-                setSelectedFile={() => { } } 
-                selectedFileURL={''}
-                setSelectedFileURL={() => { } } 
+                setSelectedFile={setSelectedFile}
+                selectedFileURL={selectedFileURL}
+                setSelectedFileURL={setSelectedFileURL}
             >
-                <ActionInfoInputs 
-                    data={data[3].actionForm}
+                <ActionInfoInputs
+                    data={data[1].actionForm}
                     title={title}
-                    setTitle={setTitle} 
+                    setTitle={setTitle}
                 />
             </ActionForm>
-            {loading ? 
-                <Grid container justifyContent="center" alignItems="center" style={{ height: "70vh" }}>
-                    {/* <CircularProgress className="flex align-center justify-center"/> */}
-                    <span>This subpage is under maintenance.</span>
-                </Grid>
-                : <Table columns={columns as any} rows={rows} handleOpenActionForm={handleOpen} totalRows={0} loading={false} paginationModel={{page: 1, pageSize: 2}}
-                    setPaginationModel={() => {}} 
-                />}
+                <Table
+                    columns={columns as any}
+                    rows={rows}
+                    handleOpenActionForm={() => setOpenActionForm(true)}
+                    loading={loading}
+                    totalRows={totalRows}
+                    paginationModel={{ page: paginationModel.page - 1, pageSize: paginationModel.pageSize }}
+                    setPaginationModel={setPaginationModel}
+                />
         </Dashboard>
     )
 }
